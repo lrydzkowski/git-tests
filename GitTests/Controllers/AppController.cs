23767c7855876d45de6d0346100e9c4480834199
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-08-31 12:34:43 +0200

    init

---
+++ GitTests/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitTests.Controllers;
+
+[ApiController]
+[Route("api/app")]
+public class AppController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAppInfo()
+    {
+        return Ok(
+            new
+            {
+                App = "GitTests"
+            }
+        );
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# GitTests
+
+A simple ASP.NET Core Web API project for testing GitHub Actions workflows.
