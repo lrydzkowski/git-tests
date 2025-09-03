ee1f76833966f273e4b3eec0d8f37b78f88d87a6
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-09-03 16:44:48 +0200

    v16

--- GitTests/Controllers/AppController.cs
+++ GitTests/Controllers/AppController.cs
@@ -13,7 +13,7 @@ public class AppController : ControllerBase
             new
             {
                 App = "GitTests",
-                Version = "v15",
+                Version = "v16",
                 Test = "2"
             }
         );
